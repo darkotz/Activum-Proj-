b2d622bc50475e75c8febc5ea7b9c26fd57d49a8
Nikita <124080539+darkotz@users.noreply.github.com>
2026-02-25 14:45:34 +0100

    Customer and insurance classes added

---
+++ PRG1_MAUI_ERP_Activum/Model/Customers.cs
@@ -0,0 +1,20 @@
+﻿namespace PRG1_MAUI_ERP_Activum.Model
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public List<Guid> InsuranceId { get; set; } = new();
+
+        public Customer(string firstName, string lastName, string email, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
---
+++ PRG1_MAUI_ERP_Activum/Model/Insurances.cs
@@ -0,0 +1,19 @@
+﻿namespace PRG1_MAUI_ERP_Activum.Model
+{
+    public class Insurance
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string InsuranceType { get; set; } = "";
+        public decimal MonthlyCost { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Insurance(string insuranceType, decimal monthlyCost)
+        {
+            InsuranceType = insuranceType;
+            MonthlyCost = monthlyCost;
+            StartDate = DateTime.Now;
+            IsActive = true;
+        }
+    }
+}
