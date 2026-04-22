a7f036355e4fdc9d2c0f1c075014d812600f96f9
Bushra741 <bushrabutt149@gmail.com>
2026-04-22 14:30:21 +0200

    reseted back again

---
+++ PRG1_MAUI_ERP_Activum/Model/Customers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG1_MAUI_ERP_Activum.Model
+{
+    class Customers
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); 
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public List<Guid> InsuranceId { get; set; } = new(); 
+
+        public Customers(string firstName, string lastName, string email, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+
+        }
+    }
+}
